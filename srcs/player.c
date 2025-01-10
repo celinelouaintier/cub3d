a82a258abb395dae5ea3806208ddcf463920c6ea
Céline Louaintier <celine.louaintier@gmail.com>
2025-01-10 15:24:32 +0100

    player_move update (je sais pas trop si c'est mieux)

--- srcs/player.c
+++ srcs/player.c
@@ -6,51 +6,51 @@
 /*   By: clouaint <clouaint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:22:38 by clouaint          #+#    #+#             */
-/*   Updated: 2025/01/10 14:53:51 by clouaint         ###   ########.fr       */
+/*   Updated: 2025/01/10 15:21:34 by clouaint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 // x = position horizontale de base de la souris sur la fenetre, c'est la mlx qui calcule ca
-int	mouse_move(int x, int y, t_data *data)
-{
-	int			delta_x;
-
-	(void)y;
-	if (x != WIDTH / 2)
-	{
-		delta_x = x - WIDTH / 2;
-		data->angle -= delta_x * 0.001;
-		if (data->angle < 0)
-			data->angle += 2 * M_PI;
-		if (data->angle > 2 * M_PI)
-			data->angle -= 2 * M_PI;
-		mlx_mouse_move(data->mlx, data->window, WIDTH / 2, HEIGHT / 2);
-	}
-	return (0);
-}
-
-// ------ PAS TOUCHE ------ //
 // int	mouse_move(int x, int y, t_data *data)
 // {
 // 	int			delta_x;
 
 // 	(void)y;
-// 	if (data->last_x == -1)
+// 	if (x != WIDTH / 2)
 // 	{
-// 		data->last_x = x;
-// 		return (0);
+// 		delta_x = x - WIDTH / 2;
+// 		data->angle -= delta_x * 0.001;
+// 		if (data->angle < 0)
+// 			data->angle += 2 * M_PI;
+// 		if (data->angle > 2 * M_PI)
+// 			data->angle -= 2 * M_PI;
+// 		mlx_mouse_move(data->mlx, data->window, WIDTH / 2, HEIGHT / 2);
 // 	}
-// 	delta_x = x - data->last_x;
-// 	data->angle -= delta_x * 0.003;
-// 	if (data->angle < 0)
-// 		data->angle += 2 * M_PI;
-// 	if (data->angle > 2 * M_PI)
-// 		data->angle -= 2 * M_PI;
-// 	data->last_x = x;
 // 	return (0);
 // }
+
+// ------ PAS TOUCHE ------ //
+int	mouse_move(int x, int y, t_data *data)
+{
+	int			delta_x;
+
+	(void)y;
+	if (data->last_x == -1)
+	{
+		data->last_x = x;
+		return (0);
+	}
+	delta_x = x - data->last_x;
+	data->angle -= delta_x * 0.003;
+	if (data->angle < 0)
+		data->angle += 2 * M_PI;
+	if (data->angle > 2 * M_PI)
+		data->angle -= 2 * M_PI;
+	data->last_x = x;
+	return (0);
+}
 
 int	key_press(int keycode, t_data *data)
 {
@@ -79,38 +79,86 @@ int	key_release(int keycode, t_data *data)
 		data->key.d = 0;
 	return (0);
 }
-
-int player_move(t_data *data)
+int	player_move(t_data *data)
 {
+	float	new_x;
+	float	new_y;
 	
+	new_x = data->player_x;
+	new_y = data->player_y;
 	if (data->key.w)
 	{
-		if(data->map.map[(int)data->player_y][(int)(data->player_x + data->raycast.dir_x * 0.1)] != '1')
-			data->player_x += data->raycast.dir_x * 0.1;
-		if(data->map.map[(int)(data->player_y + data->raycast.dir_y * 0.1)][(int)data->player_x] != '1')
-			data->player_y += data->raycast.dir_y * 0.1;
+		new_x = data->player_x + data->raycast.dir_x * 0.1;
+		new_y = data->player_y + data->raycast.dir_y * 0.1;
+		if(data->map.map[(int)new_y][(int)new_x] != '1')
+		{
+			data->player_x = new_x;
+			data->player_y = new_y;
+		}
 	}
 	if (data->key.s)
 	{
-		if(data->map.map[(int)data->player_y][(int)(data->player_x - data->raycast.dir_x * 0.1)] != '1')
-			data->player_x -= data->raycast.dir_x * 0.1;
-		if(data->map.map[(int)(data->player_y - data->raycast.dir_y * 0.1)][(int)data->player_x] != '1')
-			data->player_y -= data->raycast.dir_y * 0.1;
+		new_x = data->player_x - data->raycast.dir_x * 0.1;
+		new_y = data->player_y - data->raycast.dir_y * 0.1;
+		if(data->map.map[(int)new_y][(int)new_x] != '1')
+		{
+			data->player_x = new_x;
+			data->player_y = new_y;
+		}
 	}
 	if (data->key.d)
 	{
-		if(data->map.map[(int)data->player_y][(int)(data->player_x + data->raycast.plane_x * 0.1)] != '1')
-			data->player_x += data->raycast.plane_x * 0.1;
-		if(data->map.map[(int)(data->player_y + data->raycast.plane_y * 0.1)][(int)data->player_x] != '1')
-			data->player_y += data->raycast.plane_y * 0.1;
+		new_x = data->player_x + data->raycast.plane_x * 0.1;
+		new_y = data->player_y + data->raycast.plane_y * 0.1;
+		if(data->map.map[(int)new_y][(int)new_x] != '1')
+		{
+			data->player_x = new_x;
+			data->player_y = new_y;
+		}
 	}
 	if (data->key.a)
 	{
-		if(data->map.map[(int)data->player_y][(int)(data->player_x - data->raycast.plane_x * 0.1)] != '1')
-			data->player_x -= data->raycast.plane_x * 0.1;
-		if(data->map.map[(int)(data->player_y - data->raycast.plane_y * 0.1)][(int)data->player_x] != '1')
-			data->player_y -= data->raycast.plane_y * 0.1;
+		new_x = data->player_x - data->raycast.plane_x * 0.1;
+		new_y = data->player_y - data->raycast.plane_y * 0.1;
+		if(data->map.map[(int)new_y][(int)new_x] != '1')
+		{
+			data->player_x = new_x;
+			data->player_y = new_y;
+		}
 	}
-	// mlx_put_image_to_window(data->mlx, data->window, data->img.img, 0, 0);
 	return (0);
 }
+
+// int player_move(t_data *data)
+// {
+	
+// 	if (data->key.w)
+// 	{
+// 		if(data->map.map[(int)data->player_y][(int)(data->player_x + data->raycast.dir_x * 0.1)] != '1')
+// 			data->player_x += data->raycast.dir_x * 0.1;
+// 		if(data->map.map[(int)(data->player_y + data->raycast.dir_y * 0.1)][(int)data->player_x] != '1')
+// 			data->player_y += data->raycast.dir_y * 0.1;
+// 	}
+// 	if (data->key.s)
+// 	{
+// 		if(data->map.map[(int)data->player_y][(int)(data->player_x - data->raycast.dir_x * 0.1)] != '1')
+// 			data->player_x -= data->raycast.dir_x * 0.1;
+// 		if(data->map.map[(int)(data->player_y - data->raycast.dir_y * 0.1)][(int)data->player_x] != '1')
+// 			data->player_y -= data->raycast.dir_y * 0.1;
+// 	}
+// 	if (data->key.d)
+// 	{
+// 		if(data->map.map[(int)data->player_y][(int)(data->player_x + data->raycast.plane_x * 0.1)] != '1')
+// 			data->player_x += data->raycast.plane_x * 0.1;
+// 		if(data->map.map[(int)(data->player_y + data->raycast.plane_y * 0.1)][(int)data->player_x] != '1')
+// 			data->player_y += data->raycast.plane_y * 0.1;
+// 	}
+// 	if (data->key.a)
+// 	{
+// 		if(data->map.map[(int)data->player_y][(int)(data->player_x - data->raycast.plane_x * 0.1)] != '1')
+// 			data->player_x -= data->raycast.plane_x * 0.1;
+// 		if(data->map.map[(int)(data->player_y - data->raycast.plane_y * 0.1)][(int)data->player_x] != '1')
+// 			data->player_y -= data->raycast.plane_y * 0.1;
+// 	}
+// 	return (0);
+// }
